312445a66db14b5e9e591791fe10cc0080195018
Leo <107321001+lmarchyok@users.noreply.github.com>
2023-05-22 14:14:37 -0700

    Update plot_2_vars.py
    added landmask

--- scripts/data visualization/plot_2_vars.py
+++ scripts/data visualization/plot_2_vars.py
@@ -64,13 +64,18 @@ class ICEsat_2_Extractor:
 # Takes arrays from h5 files and plots variables
 class ICEsat_2_Visualizer:
 
-    def __init__(self, raw_data_dir, dest_dir, var1, var2):
+    def __init__(self, raw_data_dir, dest_dir, var1, var2, loc_id, water_type="ALL", agg_by_intensity=True, split_by_intensity=True, agg_by_gtx=False):
       self.raw_data_dir = raw_data_dir
       self.dest_dir = dest_dir
       self.var1 = var1
       self.var2 = var2
+      self.loc_id = loc_id
+      self.water_type = water_type
+      self.agg_by_intensity = agg_by_intensity 
       self.beams = ['gt1l', 'gt1r', 'gt2l', 'gt2r', 'gt3l', 'gt3r']
       self.ag_beams = ['gt1', 'gt2', 'gt3']
+      self.intensities = ['high', 'low']
+      self.split_by_intensity = split_by_intensity
 
 
     def get_arrays(self):
@@ -136,6 +141,92 @@ class ICEsat_2_Visualizer:
 
       return [(getattr(self, f'{beam}_var1_arr'), getattr(self, f'{beam}_var2_arr')) for beam in self.beams]
     
+    def get_12_arrays_w_landmask(self):
+
+      self.high_beams_var1 = []
+      self.high_beams_var2 = []
+      self.low_beams_var1 = []
+      self.low_beams_var2 = []
+
+      progress = 1
+
+      for file in os.listdir(self.dest_dir):
+
+        print(f"Progress: {progress}/{len(os.listdir(self.dest_dir))}") #, self.dest_dir+'/'+file+'/'+os.listdir(self.dest_dir+'/'+file)[0])
+
+        data = get_dataframes((self.dest_dir+'/'+file+'/'+os.listdir(self.dest_dir+'/'+file)[0]), '/content/drive/MyDrive/ICEsat-2/Scripts/')
+        IS2_atl03_landmask = seaLand()
+        land_mask1 = IS2_atl03_landmask.label_seaLand_function(data[0], data[1], data[2], data[3], data[4], data[5], data[6])[0]
+        metadata_mask2 = IS2_atl03_landmask.label_seaLand_function(data[0], data[1], data[2], data[3], data[4], data[5], data[6], self.var1, self.var2)[1]
+
+        mask = masker(land_mask1, metadata_mask2, self.var1, self.var2)
+        masked_arrays = mask.get_masked_data(self.var1, self.var2)
+
+        intensities = []          
+
+        for k,v,s,b in masked_arrays:
+
+            intensities.append(s)
+
+            if len(k) == 0:
+              continue
+
+            if self.var1 == 'solar_elevation':
+
+              k = np.delete(k, (k > 180))
+
+            if self.var2 == 'near_sat_fract' or self.var2 == 'full_sat_fract':
+              for i in np.where(k > 180):
+
+                list_of_bad_datapoints = []
+
+                if len(i) > 0:
+                  for j in i:
+                    list_of_bad_datapoints.append(j)
+                  v = np.delete(v, list_of_bad_datapoints)
+                else:
+                  v_ = v
+
+                try:
+                  setattr(self, f'{b}_var1_arr', np.mean(k))
+                  setattr(self, f'{b}_var2_arr', np.mean(v))
+
+                  if not hasattr(self, f'{b}_var1_arr_sb'):
+                    setattr(self, f'{b}_var1_arr_sb', [])
+                  else:
+                    getattr(self, f'{b}_var1_arr_sb').append(np.mean(k))
+                  if not hasattr(self, f'{b}_var2_arr_sb'):
+                    setattr(self, f'{b}_var2_arr_sb', [])
+                  else:
+                    getattr(self, f'{b}_var2_arr_sb').append(np.mean(v))
+                  
+
+                except:
+                    continue
+        
+        setattr(self, file+'_high_beams', sorted(zip(intensities, self.beams), reverse=True)[:3][0][1][-1])
+        setattr(self, file+'_low_beams', sorted(zip(intensities, self.beams), reverse=False)[:3][0][1][-1])
+
+        for b in self.beams:
+          if b[-1] == getattr(self, file+'_high_beams'):
+            self.high_beams_var1.append(getattr(self, f'{b}_var1_arr')) 
+            self.high_beams_var2.append(getattr(self, f'{b}_var2_arr'))
+            setattr(self, f'{b}_var1_arr', [])
+            setattr(self, f'{b}_var2_arr', [])
+
+          else:
+            self.low_beams_var1.append(getattr(self, f'{b}_var1_arr'))
+            self.low_beams_var2.append(getattr(self, f'{b}_var2_arr'))
+            setattr(self, f'{b}_var1_arr', [])
+            setattr(self, f'{b}_var2_arr', [])
+
+        progress += 1
+
+      if self.agg_by_intensity:
+        return ((self.high_beams_var1, self.high_beams_var2) , (self.low_beams_var1, self.low_beams_var2))
+      else:
+        return [(getattr(self, f'{b}_var1_arr'), getattr(self, f'{b}_var2_arr')) for b in self.beams]
+    
     
     def plot_2_vars(laser_id, var1, var2, var1_arr, var2_arr, selims='auto'):
 
@@ -170,48 +261,82 @@ class ICEsat_2_Visualizer:
       plt.close()
 
 
-    def plot_all_lasers(self, arrays, selims='auto'):
+    def plot_all_lasers(self, arrays:tuple, specific_intensity='high', selims='auto'):
 
       setattr(self, 'BIG_var1_arr', np.empty(0,dtype=np.float64))
       setattr(self, 'BIG_var2_arr', np.empty(0,dtype=np.float64))
 
-      for k,v in arrays:
-        print(k,v)
-        self.BIG_var1_arr = np.concatenate([self.BIG_var1_arr, k])
-        self.BIG_var2_arr = np.concatenate([self.BIG_var2_arr, v])
+      if self.agg_by_intensity and not self.split_by_intensity:
+        for k,v in arrays:
+          self.BIG_var1_arr = np.concatenate([self.BIG_var1_arr, np.array([x for x in k if type(x) != type([])])])
+          self.BIG_var2_arr = np.concatenate([self.BIG_var2_arr, np.array([x for x in v if type(x) != type([])])])
+
+      elif self.split_by_intensity:
+        print(arrays)
+        self.BIG_var1_arr = np.concatenate([self.BIG_var1_arr, np.array([x for x in arrays[0] if type(x) != type([])])])
+        self.BIG_var2_arr = np.concatenate([self.BIG_var2_arr, np.array([x for x in arrays[1] if type(x) != type([])])])
+        print(self.BIG_var1_arr)
+        print(self.BIG_var2_arr)
+
+      else:
+        for k,v in arrays:
+            self.BIG_var1_arr = np.concatenate([self.BIG_var1_arr, np.array([x for x in k if type(x) != type([])])])
+            self.BIG_var2_arr = np.concatenate([self.BIG_var2_arr, np.array([x for x in v if type(x) != type([])])])
 
       if selims=='auto':
             lower = np.nanmin(self.BIG_var2_arr)
             upper = np.nanmax(self.BIG_var2_arr)
             serange = upper - lower
             selims = [lower - 0.2*serange, upper + 0.1*serange]
+            print(selims)
 
       lat_lower = np.nanmin(self.BIG_var1_arr)
       lat_upper = np.nanmax(self.BIG_var1_arr)
       latrange = lat_upper - lat_lower
       latlims = [lat_lower - 0.2 * latrange, lat_upper + 0.1 * latrange]
+      print(latlims)
 
       fig = plt.figure(figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
       ax = fig.add_subplot(111)
-      ax.set_title(f'{self.var1} vs {self.var2} over entire track ALL Lasers')
+      ax.set_title(f'Mean {self.var2} vs {self.var1} - {specific_intensity} intensity - NC data')
       ax.set_xlabel('Solar Elevation (degrees)')
       ax.set_ylabel('Saturation (percentage)')
 
       colors = ['r','g','b','c','m','y']
 
-      for beam in self.beams:
-        print(beam)
-        setattr(self, f'{beam}', ax.scatter(np.array(getattr(self, f'{beam}_var1_arr')), np.array(getattr(self, f'{beam}_var2_arr')), s=10,c=colors[self.beams.index(beam)],edgecolors='none',label=beam))
+      if self.agg_by_intensity and not self.split_by_intensity:
+        for intensity in self.intensities:
 
-      ax.legend(handles=[self.gt1l,self.gt1r,self.gt2l,self.gt2r,self.gt3l,self.gt3r], loc='lower left')
-      ax.set_xlim(latlims)
-      ax.set_ylim(selims)
+          setattr(self, f'{intensity}_beams_var1', np.array([x for x in getattr(self, f'{intensity}_beams_var1') if type(x) != type([])]))
+          setattr(self, f'{intensity}_beams_var2', np.array([x for x in getattr(self, f'{intensity}_beams_var2') if type(x) != type([])]))
+          setattr(self, f'{intensity}', ax.scatter(np.array(getattr(self, f'{intensity}_beams_var1')), np.array(getattr(self, f'{intensity}_beams_var2')), s=10,c=colors[self.intensities.index(intensity)],edgecolors='none',label=intensity))
+
+        ax.legend(handles=[self.high,self.low], loc='lower left')
+        ax.set_xlim(latlims)
+        ax.set_ylim(selims)
+      
+      elif self.split_by_intensity: 
+
+        setattr(self, f'{specific_intensity}_beams_var1', np.array([x for x in getattr(self, f'{specific_intensity}_beams_var1') if type(x) != type([])]))
+        setattr(self, f'{specific_intensity}_beams_var2', np.array([x for x in getattr(self, f'{specific_intensity}_beams_var2') if type(x) != type([])]))
+        setattr(self, f'{specific_intensity}', ax.scatter(np.array(getattr(self, f'{specific_intensity}_beams_var1')), np.array(getattr(self, f'{specific_intensity}_beams_var2')), s=10,c=colors[self.intensities.index(specific_intensity)],edgecolors='none',label=specific_intensity))
+
+        ax.legend(handles=[getattr(self, f'{specific_intensity}')], loc='lower left')
+        ax.set_xlim(latlims)
+        ax.set_ylim(selims)
+
+      else:
+        for beam in self.beams:
+          setattr(self, f'{beam}', ax.scatter(np.array(getattr(self, f'{beam}_var1_arr')), np.array(getattr(self, f'{beam}_var2_arr')), s=10,c=colors[self.beams.index(beam)],edgecolors='none',label=beam))
+
+        ax.legend(handles=[self.gt1l,self.gt1r,self.gt2l,self.gt2r,self.gt3l,self.gt3r], loc='lower left')
+        ax.set_xlim(latlims)
+        ax.set_ylim(selims)
+
+      print("done")
 
       if not os.path.exists(f'figs/aggregate/{self.var1}_vs_{self.var2}'):
           os.makedirs(f'figs/aggregate/{self.var1}_vs_{self.var2}')
-      fn = f'figs/aggregate/{self.var1}_vs_{self.var2}/six_plots.png'
+      fn = f'figs/aggregate/{self.var1}_vs_{self.var2}/{self.loc_id}_{self.water_type}.png'
       plt.savefig(fn, dpi=150)
       plt.close()
-
-
-
